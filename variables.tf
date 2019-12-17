2e3effaeceaaf8fe31563ffd23717f8c72834ca3
adrian-smaranda12 <58935226+adrian-smaranda12@users.noreply.github.com>
2019-12-17 15:45:56 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-variable "user_email" {
-  default = ""
-}
-
-variable "api_key" {
-  default = ""
-}
-
-
-variable "endpoint" {
-  default ="https://api.bigstep.com/metal-cloud"
-}
 
 variable "datacenter" {
   default="uk-reading"
